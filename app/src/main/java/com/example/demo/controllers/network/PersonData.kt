6c7f380b873ca65dca679391210f8f24341f28bb
Vivek Padmanabhan <vivek@bankoncube.com>
2022-03-05 10:14:46 +0530

    Refactor

--- app/src/main/java/com/example/demo/controllers/network/PersonData.kt
+++ app/src/main/java/com/example/demo/controllers/network/PersonData.kt
@@ -1,4 +1,4 @@
 package com.example.demo.controllers.network
 
-class PersonAPI {
+class PersonData {
 }
--- app/src/main/java/com/example/demo/views/viewmodel/PersonViewModel.kt
+++ app/src/main/java/com/example/demo/views/viewmodel/PersonViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.demo.views.data
+package com.example.demo.views.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class PersonData: ViewModel() {
+class PersonViewModel: ViewModel() {
     private val _name = MutableLiveData("Vivek")
 
     val name: LiveData<String> = _name
