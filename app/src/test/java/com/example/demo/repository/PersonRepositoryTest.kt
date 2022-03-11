700ff28fb2dc8a20ce21004a6af00c6ad89b699c
Vivek Padmanabhan <vivek@bankoncube.com>
2022-03-11 06:17:19 +0530

    Tests

--- app/src/test/java/com/example/demo/repository/PersonRepositoryTest.kt
+++ app/src/test/java/com/example/demo/repository/PersonRepositoryTest.kt
@@ -2,20 +2,28 @@ package com.example.demo.repository
 
 import org.junit.Test
 import org.junit.Assert.*
-
-import kotlinx.coroutines.runBlocking
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.doReturn
+import kotlinx.coroutines.runBlocking
 
 import com.example.demo.models.data.entity.Person
 import com.example.demo.models.network.entity.PersonResponse
 import com.example.demo.models.network.service.PersonService
 import com.example.demo.models.data.repository.PersonRepository
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.stub
 
 class PersonRepositoryTest {
     @Test
-    fun checkRepository() = runBlocking {
+    fun checkRepository() {
+        val personResponse = PersonResponse("1", "Test")
+        val personService = mock<PersonService> {
+            onBlocking { search() } doReturn personResponse
+        }
 
+        val personRepository = PersonRepository(personService)
+        val person: Person = runBlocking {
+            personRepository.search()
+        }
+
+        assertEquals("Test", person.name)
     }
 }
